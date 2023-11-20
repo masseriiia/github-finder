28ffef7e472a376bd159021a0b4c4cbbf657f8d4
Danil <bloodshoww@mail.ru>
2023-11-20 14:44:56 +0300

    bugfix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,30 +2,34 @@ import React from 'react';
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import axios from "axios";
-import { Octokit} from "octokit";
-import token from "../utils/process.env"
+import { Octokit } from "@octokit/rest";
+
 
 
 const Home = () => {
 
-
-
     React.useEffect(() => {
         const fetchData = async () => {
             const octokit = new Octokit({
-                auth: token
+                auth: process.env.REACT_APP_GITHUB_TOKEN
             })
             try {
-
-                const responce = await octokit.request('GET /users/{username}', {
-                    username: 'masseriiia',
+                await octokit.request("GET /user/starred/{owner}/{repo}", {
+                    owner: "github",
+                    repo: "docs",
                     headers: {
-                        'X-GitHub-Api-Version': '2022-11-28'
-                    }
-                })
-                console.log(responce.data)
+                        "x-github-api-version": "2022-11-28",
+                    },
+                });
+
+                console.log(`The repository is starred by me`);
+
             } catch (error) {
-                console.error("Error:", error)
+                if (error.status === 404) {
+                    console.log(`The repository is not starred by me`);
+                } else {
+                    console.error(`An error occurred while checking if the repository is starred: ${error?.response?.data?.message}`);
+                }
             }
         }
         fetchData()
